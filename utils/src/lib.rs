4056a9c74d5ece1a281249fc4ff633addc3a67cc
carsonzhu <carsonzhu@tencent.com>
2022-01-07 14:59:10 +0800

    fix random_double

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -10,8 +10,7 @@ pub fn degrees_to_radians(degrees: f64) -> f64 {
 /// Returns a random real in [0, 1).
 pub fn random_double() -> f64 {
     let mut rng = rand::thread_rng();
-    let r = rng.gen::<f64>();
-    r / (INFINITY + 1.)
+    rng.gen::<f64>()
 }
 
 /// Returns a random real in [min, max).
@@ -27,4 +26,16 @@ pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
         return max;
     }
     x
+}
+
+#[cfg(test)]
+mod test {
+    use crate::random_double;
+
+    #[test]
+    fn test_random_double() {
+        let v1 = random_double();
+        let v2 = random_double();
+        assert_ne!(v1, v2);
+    }
 }
